a91b238018a972dd72c1b1bdf96bbd61f72a1855
yyeung13 <33860110+yyeung13@users.noreply.github.com>
2020-03-24 12:14:27 +0800

    Update CustomerRepository.java

--- src/main/java/demo/bank/customer/repository/CustomerRepository.java
+++ src/main/java/demo/bank/customer/repository/CustomerRepository.java
@@ -8,4 +8,6 @@ import demo.bank.customer.model.Customer;
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, String> {
 
+  @Query("SELECT t FROM Customer c where c.name = ?1 AND c.ic = ?2 AND c.bank = ?3")
+	public Optional<Todo> findByNameAndICAndBank(String name, String ic, String bank);
 }
